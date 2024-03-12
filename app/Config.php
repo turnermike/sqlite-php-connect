4c00f06442275543167c8c5fe54ab5a14cca26bd
Mike Turner <turner.mike@gmail.com>
2024-03-12 08:05:10 -0400

    added try/catch

--- app/Config.php
+++ app/Config.php
@@ -11,5 +11,4 @@ class Config
    * path to the sqlite file
    */
   const PATH_TO_SQLITE_FILE = '/Users/chucknorris/Sites/Assesments/CAS/sqlite-php-connect/db/phpsqlite.db';
-  // const PATH_TO_SQLITE_FILE = __DIR__;
 }
--- app/FormHandler.php
+++ app/FormHandler.php
@@ -5,30 +5,36 @@ require '../vendor/autoload.php';
 
 use App\SQLiteConnection;
 
-// SQLite database connection
-// $db = new SQLite3('user_data.db');
-$db = (new SQLiteConnection())->connect();
+try {
 
-// Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // SQLite database connection
+  // $db = new SQLite3('user_data.db');
+  $db = (new SQLiteConnection())->connect();
 
-  $firstName = $_POST['firstName'];
-  $lastName = $_POST['lastName'];
-  $email = $_POST['email'];
+  // Check if the form is submitted
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  $query = $db->prepare('INSERT INTO Users (FirstName, LastName, Email) VALUES (:firstName, :lastName, :email)');
-  $query->bindValue(':firstName', $firstName, SQLITE3_TEXT);
-  $query->bindValue(':lastName', $lastName, SQLITE3_TEXT);
-  $query->bindValue(':email', $email, SQLITE3_TEXT);
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
 
-  $result = $query->execute();
+    $query = $db->prepare('INSERT INTO Users (FirstName, LastName, Email) VALUES (:firstName, :lastName, :email)');
+    $query->bindValue(':firstName', $firstName, SQLITE3_TEXT);
+    $query->bindValue(':lastName', $lastName, SQLITE3_TEXT);
+    $query->bindValue(':email', $email, SQLITE3_TEXT);
 
-  if ($result) {
-    echo 'Data inserted successfully!';
-    // header("Location: ../index.php");
-  } else {
-    echo 'Error inserting data.';
+    $result = $query->execute();
+
+    if ($result) {
+      echo 'Data inserted successfully!';
+      // header("Location: ../index.php");
+    } else {
+      echo 'Error inserting data.';
+    }
   }
+} catch (PDOException $e) {
+
+  echo "Connection failed: " . $e->getMessage();
 }
 
 // Close the database connection
